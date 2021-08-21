7a33c906e82c93df35931c434be0339d0c235af9
Yehia Ehab Orfy <yehiaorfy3@gmail.com>
2021-08-22 00:12:57 +0200

    Update Modules.h

--- Code/Modules.h
+++ Code/Modules.h
@@ -6,6 +6,10 @@ void Motor_BackwardDirection(int ForwardPin,int BackwardPin);
 void Motor_SetSpeed(int SpeedPin,int Speed);
 void Motor_Stop(int ForwardPin,int BackwardPin,int SpeedPin);
 
+void LED_Foraward(int ForwardInd,int BackwardInd,int StopInd);
+void LED_Backward(int ForwardInd,int BackwardInd,int StopInd);
+void LED_Stop(int ForwardInd,int BackwardInd,int StopInd);
+
 int Calculate_Time(int Angle,int Speed,int Gear);
 int Calculate_Speed(int Voltage);
 
